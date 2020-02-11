9d5e067cf1de9af3c673cc61fd1a52fa17b2bfbb
sam <samndu2@gmail.com>
2020-02-11 07:01:01 +0100

    fixed upload errors2

--- instance/config.py
+++ instance/config.py
@@ -3,5 +3,5 @@ basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
-UPLOAD_URL = os.path.join(basedir, 'user_stories\\')
+UPLOAD_URL = os.path.join(basedir, 'user_stories')
 MAX_CONTENT_LENGTH = 5*1024*1024
